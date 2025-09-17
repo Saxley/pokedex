ed5c2d776761a322d78c79c745eacbb46bbc8537
foxcornio <foxygamercompany@gmail.com>
2025-09-17 06:19:36 -0600

    Conflicts: 	app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-#import src.terminal.terminal
-from src.gui.main_window import display_input
+import src.terminal.terminal
+#from src.gui.main_window import display_input
 # Punto de entrada principal de la aplicación
 if __name__ == "__main__":
-        display_input()
-        #src.terminal.terminal.generate_static_pokemon_images()
+        #display_input()
+        src.terminal.terminal.generate_static_pokemon_images()
+
