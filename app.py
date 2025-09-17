6d0bcb6aa5f60a0150ee4a37f46be62f0d6d920b
foxcornio <foxygamercompany@gmail.com>
2025-09-17 03:36:40 -0600

    Segundo intento movil

--- app.py
+++ app.py
@@ -9,16 +9,13 @@ except ModuleNotFoundError:
 # Punto de entrada principal de la aplicación
 if __name__ == "__main__":
     if display_input:
-        try:
-            # Ejecutamos la función principal para iniciar la interfaz gráfica
-            display_input()
-        except:
-            create_gui_images()
+        # Ejecutamos la función principal para iniciar la interfaz gráfica
+        display_input()
     else:
-        # Modo terminal: solicitar el nombre del Pokémon y generar imágenes
-        pokemon_name = input("Ingrese el nombre del Pokémon: ").strip().lower()
         from src.utils.api import fetch_pokemon_data, fetch_evolution_chain
         from src.gui.main_window import create_gui_images
+        # Modo terminal: solicitar el nombre del Pokémon y generar imágenes
+        pokemon_name = input("Ingrese el nombre del Pokémon: ").strip().lower()
 
         pokemon_data = fetch_pokemon_data(pokemon_name)
         if pokemon_data:
